02b9621700291a4e02a39f9d3fb2c8553b8f8a1d
Fury <ashiqasraf07@gmail.com>
2021-10-24 19:52:19 +0530

    Modified index.js file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,82 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from './components/Header';
-import Home from './pages/Home';
-import Error from './pages/Error';
-import Loading from './pages/Loading';
-import './css/style.css';
+import "./App.scss";
 
-const App = () => {
-	const [data, setData] = useState([]);
-	const [error, setError] = useState(false);
-	const [loading, setLoading] = useState(true);
+import CountryPage from "./pages/CountryPage";
+import ErrorPage from "./pages/ErrorPage";
+import Homepage from "./pages/Homepage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-	const fetch = async () => {
-		try {
-			await axios.get('https://restcountries.com/v3/all').then((res) => setData(res.data));
-		} catch (err) {
-			setError(err ? true : false);
-		}
-	};
+localStorage.removeItem("region");
+localStorage.removeItem("countries");
 
-	useEffect(() => {
-		fetch();
-		setLoading(false);
-	}, []);
+function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-	return (
-		<div className="container">
-			<Header />
-			{loading ? <Loading /> : error ? <Error /> : <Home data={data} />}
-		</div>
-	);
-};
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://restcountries.com/v2/all")
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.message) {
+          // In case there are additional problems
+          throw new Error(json.message);
+        } else {
+          setData(json);
+          setLoading(false);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+        {!loading ? (
+          <Switch>
+            <Route exact path="/">
+              {!error ? (
+                data && <Homepage data={data} />
+              ) : (
+                <ErrorPage error={error} />
+              )}
+            </Route>
+            {data &&
+              data.map((country) => {
+                return (
+                  <Route
+                    exact
+                    path={`/${country.alpha3Code}`}
+                    key={country.alpha3Code}
+                  >
+                    <CountryPage country={country} data={data} />
+                  </Route>
+                );
+              })}
+            <Route exact path="/:code">
+              <ErrorPage error={error} />
+            </Route>
+          </Switch>
+        ) : (
+          <div className="loader-cont">
+            <div className="loader-animation"></div>
+          </div>
+        )}
+      </Router>
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
+
+// https://github.com/leonardomeza87
